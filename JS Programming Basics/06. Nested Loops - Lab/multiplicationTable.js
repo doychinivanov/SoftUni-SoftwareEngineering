600c60da43324fa54df8f2c925171403d4d68287
Doychin Ivanov <doychinivn@gmail.com>
2021-02-07 11:48:14 +0200

    Add files via upload

---
+++ JS Programming Basics/06. Nested Loops - Lab/multiplicationTable.js
@@ -0,0 +1,11 @@
+function solve(){
+
+    for(let i=1; i<=10; i++){
+        for(let k=1; k<=10;k++){
+            let result = i*k;
+            console.log(`${i} * ${k} = ${result}`);
+        }
+    }
+
+}
+solve()
