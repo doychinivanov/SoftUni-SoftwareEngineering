26a56cc5975e625716c23ad0f1ff2681d30c72bd
Doychin Ivanov <doychinivn@gmail.com>
2021-02-12 22:52:49 +0200

    Add files via upload

---
+++ JS - Fundamentals/01. Basic Syntax-Conditions-Loops Lab/excellentGrade.js
@@ -0,0 +1,11 @@
+function solve(grade){
+    grade = Number(grade);
+
+    if(grade >= 5.5){
+        console.log(`Excellent`);
+    } else {
+        console.log(`Not excellent`);
+    }
+}
+
+solve(5.50)
