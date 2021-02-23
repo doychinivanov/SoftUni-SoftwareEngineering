0b4df89b0dedfb523428361d795e841c8879bd01
Doychin Ivanov <doychinivn@gmail.com>
2021-02-24 01:28:31 +0200

    Add files via upload

---
+++ JS - Fundamentals/06. Objects and Classes - Lab/city.js
@@ -0,0 +1,14 @@
+function printCity(obj){
+    for (const key in obj) {
+       console.log(`${key} -> ${obj[key]}`);
+    }
+}
+
+printCity({
+    name: "Sofia",
+    area: 492,
+    population: 1238438,
+    country: "Bulgaria",
+    postCode: "1000"
+}
+)
