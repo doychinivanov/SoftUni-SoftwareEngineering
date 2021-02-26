780bf0b94159a544816a5c37ca09f3463a4b6a58
Doychin Ivanov <doychinivn@gmail.com>
2021-02-26 23:09:54 +0200

    Add files via upload

---
+++ JS - Fundamentals/06. Objects and Classes - Exercise/classStorage.js
@@ -0,0 +1,36 @@
+class Storage{
+    constructor(capacity){
+        this.capacity = Number(capacity);
+        this.storage = [];
+        this.totalCost = 0;
+    }
+
+    addProduct(product){
+        this.storage.push(product);
+        this.capacity -= product.quantity;
+        this.totalCost += product.price * product.quantity;
+    }
+
+    getProducts(){
+        let result = [];
+        this.storage.forEach(product =>{
+            console.log(JSON.stringify(product));
+            result.push(JSON.stringify(product));
+        })
+
+        return result.join('\n')
+    };
+}
+
+
+
+let productOne = {name: 'Cucamber', price: 1.50, quantity: 15};
+let productTwo = {name: 'Tomato', price: 0.90, quantity: 25};
+let productThree = {name: 'Bread', price: 1.10, quantity: 8};
+let storage = new Storage(50);
+storage.addProduct(productOne);
+storage.addProduct(productTwo);
+storage.addProduct(productThree);
+storage.getProducts();
+console.log(storage.capacity);
+console.log(storage.totalCost);
