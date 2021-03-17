ebfa1aaeff0c46ceac53f8005e3d740ecb8a2d89
Doychin Ivanov <doychinivn@gmail.com>
2021-03-17 21:06:30 +0200

    Add files via upload

---
+++ JS - Fundamentals/11. Regular Expressions - Lab/matchPhoneNumber.js
@@ -0,0 +1,10 @@
+function solve([input]){
+    const pattern = new RegExp('\\+359([- ])2\\1[- ]*[\\d]{3}\\1[\\d]{4}\\b', 'g');
+    const matches = input.match(pattern);
+
+    return matches.join(', ')
+}
+
+console.log(solve([
+    '+359 2 222 2222,359-2-222-2222, +359/2/222/2222, +359-2 222 2222 +359 2-222-2222, +359-2-222-222, +359-2-222-22222 +359-2-222-2222'
+  ]));
