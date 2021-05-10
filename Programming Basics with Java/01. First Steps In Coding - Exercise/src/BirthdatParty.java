98fabf0d8db2a3ac7ad079526404c16f25690d27
Doychin Ivanov <doychinivn@gmail.com>
2021-05-10 18:43:17 +0300

    solution added

--- Programming Basics with Java/01. First Steps In Coding - Exercise/src/BirthdatParty.java
+++ Programming Basics with Java/01. First Steps In Coding - Exercise/src/BirthdatParty.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class BirthdatParty {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        double rent = Double.parseDouble(scan.nextLine());
+
+        double cakePrice = rent * 0.2;
+        double animatorPrice = rent / 3;
+        double drinksPrice = cakePrice - cakePrice * 0.45;
+
+        System.out.println(rent + cakePrice + drinksPrice + animatorPrice);
+    }
 }
