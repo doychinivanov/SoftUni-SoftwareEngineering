448eed72e6d267c9ee69f1e9c80e7d41bf6de411
Doychin Ivanov <doychinivn@gmail.com>
2021-05-18 11:10:42 +0300

    solution added

--- Programming Basics with Java/06. Nested Loops - Exercise/src/TrainTrainers.java
+++ Programming Basics with Java/06. Nested Loops - Exercise/src/TrainTrainers.java
@@ -6,7 +6,25 @@ public class TrainTrainers {
 
         int jury = Integer.parseInt(scan.nextLine());
         String input = scan.nextLine();
+        double totalScore = 0;
+        int totalGrades = 0;
 
-        
+        while (input.equals("Finish") == false){
+            String task = input;
+            double currentTaskTotal = 0;
+
+            for (int i = 0; i < jury; i++) {
+                double grade = Double.parseDouble(scan.nextLine());
+                currentTaskTotal += grade;
+                totalScore += grade;
+                totalGrades++;
+            }
+
+            System.out.printf("%s - %.2f.%n", task, currentTaskTotal / jury);
+            input = scan.nextLine();
+        }
+
+        System.out.printf("Student's final assessment is %.2f.", totalScore / totalGrades);
+
     }
 }
