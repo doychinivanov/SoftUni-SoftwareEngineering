47f927b725e6f720d69b40c7f9d9dc545f0bc536
Doychin Ivanov <doychinivn@gmail.com>
2021-06-02 20:40:09 +0300

    list lab

---
+++ Java Fundamentals/05. Lists - Lab/src/SumEqualNums.java
@@ -0,0 +1,36 @@
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class SumEqualNums {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        List<Double> numbers = Arrays
+                .stream(scan.nextLine().split(" "))
+                .map(Double::parseDouble)
+                .collect(Collectors.toList());
+
+        for (int i = 0; i < numbers.size() - 1; i++) {
+            if(numbers.get(i).equals(numbers.get(i + 1))){
+                numbers.set(i, numbers.get(i) + numbers.get(i + 1));
+                numbers.remove(i + 1);
+                i = -1;
+            }
+        }
+
+        System.out.println(joinElements(numbers, " "));
+    }
+
+    static String joinElements(List<Double> items, String delimiter){
+        String output = "";
+
+        for(Double item: items){
+            output += new DecimalFormat("0.#").format(item) + delimiter;
+        }
+
+        return output;
+    }
+}
