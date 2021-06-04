3c33be78be31c2807550d9c9ea5489dd5ceb85ab
Doychin Ivanov <doychinivn@gmail.com>
2021-06-04 12:57:39 +0300

    more tasks

--- JS Advanced/04. DOM Introduction - Exercise/05. Table - Search Engine/solution.js
+++ JS Advanced/04. DOM Introduction - Exercise/05. Table - Search Engine/solution.js
@@ -2,7 +2,15 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      const tableData = [...document.querySelectorAll('table tbody tr')];
+      const searchParams = document.getElementById('searchField').value;
 
+      tableData.forEach(data => {
+         if(data.textContent.toLowerCase().includes(searchParams.toLowerCase())){
+            data.classList.add('select');
+         } else {
+            data.classList.remove('select');
+         }
+      })
    }
 }
