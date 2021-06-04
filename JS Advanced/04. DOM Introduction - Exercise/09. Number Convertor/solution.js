e304d57564cc993c8192407591c505f4bf3364dc
Doychin Ivanov <doychinivn@gmail.com>
2021-06-04 15:44:43 +0300

    all taks complited

--- JS Advanced/04. DOM Introduction - Exercise/09. Number Convertor/solution.js
+++ JS Advanced/04. DOM Introduction - Exercise/09. Number Convertor/solution.js
@@ -1,4 +1,40 @@
 function solve() {
+    const selectElement = document.querySelector('#selectMenuTo');
+    createOptions();
+    
+    const options = Array.from(document.querySelectorAll('#selectMenuTo>option'))
+    const input = document.getElementById('input');
+    const output = document.getElementById('result')
 
-    //TODO...
+    document.querySelector('button').addEventListener('click', ()=>{
+        const chosen = options.filter(x => x.selected)[0].value
+        const result = convertDecimal(Number(input.value), chosen);
+        
+        output.value = result;
+    });
+
+
+    function convertDecimal(number, toType){
+        const actions = {
+            binary: ()=>{
+                return number.toString(2)
+            },
+            hexadecimal: ()=>{
+                return number.toString(16).toUpperCase();
+            }
+        }
+
+        return actions[toType]();
+    }
+
+    function createOptions(){
+        const binaryOption = document.createElement('option');
+        const hexadecimalOption = document.createElement('option');
+        binaryOption.value = 'binary';
+        binaryOption.text = 'Binary';
+        hexadecimalOption.value = 'hexadecimal';
+        hexadecimalOption.text = 'Hexadecimal';
+        selectElement.add(binaryOption);
+        selectElement.add(hexadecimalOption);
+    }
 }
