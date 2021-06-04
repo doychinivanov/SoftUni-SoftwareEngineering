ac66414dee6fd76093dbf75da2f001f0b20e65c6
Doychin Ivanov <doychinivn@gmail.com>
2021-06-04 12:27:47 +0300

    more solutions added

--- JS Advanced/04. DOM Introduction - Exercise/03. Accordion/accordion.js
+++ JS Advanced/04. DOM Introduction - Exercise/03. Accordion/accordion.js
@@ -1,3 +1,7 @@
 function toggle() {
-    console.log('TODO:...');
+    const moreInfo = document.getElementById('extra');
+    const button = document.querySelector('.button');
+
+    moreInfo.style.display = moreInfo.style.display == 'none' ? 'block' : 'none';
+    button.textContent = moreInfo.style.display == 'none' ? 'More' : 'Less';
 }
--- JS Advanced/04. DOM Introduction - Exercise/04. Search in List/search.js
+++ JS Advanced/04. DOM Introduction - Exercise/04. Search in List/search.js
@@ -1,3 +1,17 @@
 function search() {
-   // TODO
+   const towns = [...document.querySelectorAll('#towns li')];
+   const searchParams = document.getElementById('searchText').value;
+
+   towns.forEach(town=>{
+      if(town.textContent.toLowerCase().includes(searchParams.toLowerCase())){
+         town.style.fontWeight = 'bold';
+         town.style.textDecoration = 'underline';
+      } else {
+         town.style.fontWeight = '';
+         town.style.textDecoration = 'none';
+      }
+   })
+
+   const matchesAmount = towns.filter(x => x.textContent.toLowerCase().includes(searchParams.toLowerCase())).length;
+   document.getElementById('result').textContent = `${matchesAmount} matches found`
 }
