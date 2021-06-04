ad709e522a8e258cbe34d143034f16feeb94824f
Doychin Ivanov <doychinivn@gmail.com>
2021-06-04 15:17:04 +0300

    more tasks added

--- JS Advanced/04. DOM Introduction - Exercise/08. Generate Report/report.js
+++ JS Advanced/04. DOM Introduction - Exercise/08. Generate Report/report.js
@@ -1,3 +1,26 @@
 function generateReport() {
-    //TODO
+    const output = document.getElementById('output');
+    const topics = Array.from(document.querySelectorAll('thead>tr>th'));
+    const rows = Array.from(document.querySelectorAll('tbody>tr'));
+
+    const indexes = [];
+    const result = [];
+    
+    topics.forEach((tr, index) => tr.querySelector('input').checked == true ? indexes.push(index) : '');
+
+    rows.forEach(row=>{
+        const selectedData = {};
+
+        indexes.forEach(index => {
+            selectedData[topics[index].querySelector('input').name] = [...row.querySelectorAll('td')][index].textContent;
+        });
+
+        result.push(selectedData);
+    })
+
+    if(indexes.length > 0){
+        output.value = JSON.stringify(result);
+    } else {
+        output.value = '';
+    }
 }
