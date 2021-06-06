6bfbbc8db352723c8b18b1f52950b5b7345cfa50
Doychin Ivanov <doychinivn@gmail.com>
2021-06-06 21:10:43 +0300

    solution added

--- JS Advanced/05. DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
+++ JS Advanced/05. DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    console.log('TODO:...');
+    document.getElementById('email').addEventListener('change', validate);
+
+    function validate(ev){
+        const regexp = /^[a-z]+@[a-z]+\.[a-z]+$/g;
+        
+        if(regexp.test(ev.target.value)){
+            ev.target.classList.remove('error')
+        } else {
+            ev.target.classList.add('error')
+        }
+    }
 }
