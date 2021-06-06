eb78f9853e85948933bc43f3f5cdc22617d300b8
Doychin Ivanov <doychinivn@gmail.com>
2021-06-06 21:31:43 +0300

    lab finished

--- JS Advanced/05. DOM Manipulation and Events - Lab/07. Shopping-Cart/solution.js
+++ JS Advanced/05. DOM Manipulation and Events - Lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,33 @@
 function solve() {
-   //TODO...
+   document.querySelector('.shopping-cart').addEventListener('click', createShoppingCart);
+   document.querySelector('.checkout').addEventListener('click', checkOut);
+   const resultField = document.querySelector('textarea');
+
+
+   const boughtProducts = {
+      totalPrice: 0,
+      items: []
+   }
+
+   function createShoppingCart(ev){
+      if(ev.target.tagName == 'BUTTON' && ev.target.classList.contains('add-product')){
+         const itemDiv = ev.target.parentNode.parentNode;
+         const productName = itemDiv.querySelector('.product-title').textContent;
+         const productPrice = Number(itemDiv.querySelector('.product-line-price').textContent);
+
+         boughtProducts.totalPrice += productPrice;
+
+         if(boughtProducts.items.includes(productName) == false){
+            boughtProducts.items.push(productName);
+         }
+
+         resultField.value += `Added ${productName} for ${productPrice.toFixed(2)} to the cart.\n`
+      }
+   }
+
+   function checkOut(){
+      resultField.value += `You bought ${boughtProducts.items.join(', ')} for ${boughtProducts.totalPrice.toFixed(2)}.`
+      document.querySelector('.shopping-cart').removeEventListener('click', createShoppingCart);
+      document.querySelector('.checkout').removeEventListener('click', checkOut);
+   }
 }
