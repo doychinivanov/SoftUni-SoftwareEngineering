7471a4038e220509450ed6b4745e7940b201f622
Doychin Ivanov <doychinivn@gmail.com>
2021-06-06 21:02:44 +0300

    progress update

--- JS Advanced/05. DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
+++ JS Advanced/05. DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
@@ -1,3 +1,14 @@
 function focused() {
-    console.log('TODO:...');
+    Array.from(document.querySelectorAll('input')).forEach(input=>{
+        input.addEventListener('blur', onBlur);
+        input.addEventListener('focus', onFocus);
+    });
+
+    function onBlur(ev){
+        ev.target.parentNode.classList.remove('focused');
+    };
+
+    function onFocus(ev){
+        ev.target.parentNode.classList.add('focused');
+    }
 }
