c0a28378eafba2f4c59ca76ed38eec991b82daaf
Doychin Ivanov <doychinivn@gmail.com>
2021-06-07 15:28:11 +0300

    exercise done

--- JS Advanced/05. DOM Manipulation and Events - Exercise/09. JavaScript-Quiz/solution.js
+++ JS Advanced/05. DOM Manipulation and Events - Exercise/09. JavaScript-Quiz/solution.js
@@ -1,3 +1,34 @@
 function solve() {
-  //TODO...
+  const correctAnswers = ['onclick', 'JSON.stringify()','A programming API for HTML and XML documents'];
+  const sections = Array.from(document.querySelectorAll('section'));
+  let score = 0;
+  let index = 0;
+
+  document.getElementById('quizzie').addEventListener('click', (ev)=>{
+    if(ev.target.classList.contains('answer-wrap') || ev.target.classList.contains('answer-text')){
+      const answer = ev.target.textContent.trim();
+      
+      if(answer == correctAnswers[index]){
+        score++;
+      }
+      
+      sections[index].style.display = 'none';
+      
+      if(sections.length > index + 1){
+        sections[index+1].style.display = 'block';
+      }
+      
+      index++;
+
+      if(sections[index] == undefined){
+        document.querySelector("#results").style.display = "block";
+        if (score !== 3) {
+          document.querySelector("#results h1").textContent = `You have ${score} right answers`;
+        } else {
+          document.querySelector("#results h1").textContent = "You are recognized as top JavaScript fan!";
+        }
+      }      
+    }
+  });
+
 }
