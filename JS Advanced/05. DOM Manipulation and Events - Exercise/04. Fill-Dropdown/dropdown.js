70b036d100c762dce9de873ded60a61cceac6c16
Doychin Ivanov <doychinivn@gmail.com>
2021-06-07 12:29:52 +0300

    solution added

--- JS Advanced/05. DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
+++ JS Advanced/05. DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,30 @@
 function addItem() {
-    console.log('TODO:...');
+    const selectMenu = document.getElementById('menu');
+    const optionText = document.getElementById('newItemText');
+    const optionValue = document.getElementById('newItemValue');
+
+    const option = createEl('option', optionText.value, 'value', optionValue.value);
+    selectMenu.appendChild(option);
+
+    optionText.value = '';
+    optionValue.value = '';
+
+
+    function createEl(type, content, attribute, attributeValue, styleType, styleValue){
+        const newLi = document.createElement(type);
+  
+        if(content){
+           newLi.textContent = content;
+        }
+  
+        if(attribute){
+            newLi.setAttribute(attribute, attributeValue);
+        }
+  
+        if(styleType){
+           newLi.style[styleType] = styleValue; 
+        }
+  
+        return newLi;
+    }
 }
