2bdfab73851b003b3b2b3a02a9dc3cdd5eb9bcd6
Doychin Ivanov <doychinivn@gmail.com>
2021-06-07 12:21:57 +0300

    solution added

--- JS Advanced/05. DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
+++ JS Advanced/05. DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,14 @@
 function lockedProfile() {
-    console.log('TODO...')
+    document.querySelector('#main').addEventListener('click', toggleInfo);
+
+    function toggleInfo(ev){
+        if(ev.target.tagName == 'BUTTON'){
+            const lockedCheckbox = ev.target.parentNode.querySelector('input[type=radio]:checked');
+
+            if(lockedCheckbox.value == 'unlock'){
+                ev.target.previousElementSibling.style.display = ev.target.previousElementSibling.style.display === 'block' ? 'none' : 'block';
+                ev.target.textContent = ev.target.previousElementSibling.style.display === 'none' ? 'Show more' : 'Hide it';
+            }
+        }
+    }
 }
