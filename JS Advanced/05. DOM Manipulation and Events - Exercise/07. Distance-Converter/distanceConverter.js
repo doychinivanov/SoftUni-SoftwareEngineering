c547754d536ddfb934bfeecd8bd449129f3a0f62
Doychin Ivanov <doychinivn@gmail.com>
2021-06-07 14:06:36 +0300

    solution added

--- JS Advanced/05. DOM Manipulation and Events - Exercise/07. Distance-Converter/distanceConverter.js
+++ JS Advanced/05. DOM Manipulation and Events - Exercise/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,42 @@
-function attachEventsListeners() {
-    console.log('TODO:...');
+function attachEventsListeners(){
+    document.getElementById('convert').addEventListener('click', ()=>{
+        const initialValue = Number(document.getElementById('inputDistance').value);
+        const chosenUnit = Array.from(document.querySelectorAll('#inputUnits>option')).filter(x => x.selected)[0].value;
+        const targetUnit = Array.from(document.querySelectorAll('#outputUnits>option')).filter(x => x.selected)[0].value;
+       
+        const valueInMetres = convertToMetres(initialValue, chosenUnit);    
+        document.getElementById('outputDistance').value = fromMetresToRest(valueInMetres, targetUnit);
+    });
+
+
+    function convertToMetres(value, type){
+        const relations = {
+            'km': value * 1000,
+            'm': value,
+            'cm': value / 100,
+            'mm': value / 1000,
+            'mi': value * 1609.34,
+            'yrd': value * 0.9144,
+            'ft': value * 0.3048,
+            'in': value * 0.0254,
+        };
+
+        return relations[type];
+    }
+
+    function fromMetresToRest(value, type){
+        const relations = {
+            'km': value / 1000,
+            'm': value,
+            'cm': value / 0.01,
+            'mm': value / 0.001,
+            'mi': value / 1609.34,
+            'yrd': value / 0.9144,
+            'ft': value / 0.3048,
+            'in': value / 0.0254,
+        }
+
+        return relations[type];
+    }
+
 }
