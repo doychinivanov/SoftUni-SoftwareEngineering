d1ce2f21e8e5a2ee0bc42036779c2f298e091463
doychinivanov <doychinivn@gmail.com>
2021-08-07 14:17:20 +0300

    exam prep progress

---
+++ Java Fundamentals/12.Exam Prep 2/src/FancyBarcodes.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class FancyBarcodes {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = Integer.parseInt(scan.nextLine());
+
+        String regex = "(@#+)(?<barcode>[A-Z][A-Za-z\\d]{4,}[A-Z])(@#+)";
+        Pattern pattern = Pattern.compile(regex);
+
+
+        for (int i = 0; i < n; i++) {
+            String input = scan.nextLine();
+
+            Matcher match = pattern.matcher(input);
+
+            if(match.find()){
+                String product = match.group("barcode");
+                String productGroup = findProductGroup(product);
+
+                System.out.printf("Product group: %s\n", productGroup);
+            } else {
+                System.out.println("Invalid barcode");
+            }
+        }
+    }
+
+    public static String findProductGroup(String product){
+        StringBuilder result = new StringBuilder();
+
+        String regex = "\\d";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher match = pattern.matcher(product);
+
+        while(match.find()){
+            result.append(match.group());
+        }
+
+        if (result.isEmpty()){
+            result.append("00");
+        }
+        return result.toString();
+    }
+}
