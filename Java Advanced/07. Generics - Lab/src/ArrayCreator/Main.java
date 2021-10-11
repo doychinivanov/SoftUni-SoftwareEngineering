930bd268ac8319ad824811b7eb3dc80c6a7f8b70
doychinivanov <doychinivn@gmail.com>
2021-10-11 14:08:07 +0300

    solution added

---
+++ Java Advanced/07. Generics - Lab/src/ArrayCreator/Main.java
@@ -0,0 +1,9 @@
+package ArrayCreator;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] javas = ArrayCreator.create(String.class, 10, "java");
+
+        Integer[] numbers = ArrayCreator.create(10, 73);
+    }
+}
