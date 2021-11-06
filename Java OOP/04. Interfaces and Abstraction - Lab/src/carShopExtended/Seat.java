6cf7dd09c3e389d17ea35c0af43c6bbc8b46e8ec
doychinivanov <doychinivn@gmail.com>
2021-11-06 21:47:18 +0200

    solution added

---
+++ Java OOP/04. Interfaces and Abstraction - Lab/src/carShopExtended/Seat.java
@@ -0,0 +1,20 @@
+package carShopExtended;
+
+public class Seat extends CarImpl implements Sellable{
+    private final Double price;
+
+    public Seat(String model, String color, Integer horsePower, String countryProduced, Double price){
+        super(model, color, horsePower, countryProduced);
+        this.price = price;
+    }
+
+    public Double getPrice() {
+        return this.price;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + System.lineSeparator()
+                + getModel() + " sells for " + price;
+    }
+}
