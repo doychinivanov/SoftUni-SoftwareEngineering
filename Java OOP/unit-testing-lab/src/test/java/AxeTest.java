e0fbb85b9ebf545c641ea83ada3a9ea30f41c798
doychinivanov <doychinivn@gmail.com>
2021-11-29 12:24:04 +0200

    first class is tested

---
+++ Java OOP/unit-testing-lab/src/test/java/AxeTest.java
@@ -0,0 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AxeTest {
+    private static final int AXE_ATTACK = 10;
+    private static final int AXE_DURABILITY = 10;
+
+    private Dummy dummy;
+    private Axe axe;
+    private Axe brokenAxe;
+
+    @Before
+    public void setUp() {
+        this.axe = new Axe(AXE_ATTACK, AXE_DURABILITY);
+        this.brokenAxe = new Axe(AXE_ATTACK, 0);
+        this.dummy = new Dummy(100, 100);
+    }
+
+    @Test
+    public void testAxeLosesDurabilityAfterAttack(){
+        axe.attack(dummy);
+        assertEquals(AXE_DURABILITY - 1, this.axe.getDurabilityPoints());
+    }
+
+    @Test (expected = IllegalStateException.class)
+    public void testAxeThrowsExIfNoDurabilityWhenAttacks() {
+        brokenAxe.attack(dummy);
+    }
+}
