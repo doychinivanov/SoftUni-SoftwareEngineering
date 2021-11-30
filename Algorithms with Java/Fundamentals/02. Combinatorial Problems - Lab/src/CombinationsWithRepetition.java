7ea29f890d138deeea2545e1f0b7990967bd4481
doychinivanov <doychinivn@gmail.com>
2021-11-30 15:43:45 +0200

    solutions with combinations done

---
+++ Algorithms with Java/Fundamentals/02. Combinatorial Problems - Lab/src/CombinationsWithRepetition.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class CombinationsWithRepetition {
+    private static String[] arr;
+    private static String[] slots;
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        arr = scan.nextLine().split("\\s+");
+
+        int slotsCount = Integer.parseInt(scan.nextLine());
+
+        slots = new String[slotsCount];
+
+        createCombinations(0, 0);
+
+    }
+
+    private static void createCombinations(int mainIndex, int startIndex) {
+        if(mainIndex == slots.length) {
+            print(slots);
+            return;
+        }
+
+        for (int i = startIndex; i < arr.length; i++) {
+            slots[mainIndex] = arr[i];
+
+            createCombinations(mainIndex + 1, i);
+        }
+    }
+
+    private static void print(String[] arr) {
+        System.out.println(String.join(" ", arr));
+    }
+}
