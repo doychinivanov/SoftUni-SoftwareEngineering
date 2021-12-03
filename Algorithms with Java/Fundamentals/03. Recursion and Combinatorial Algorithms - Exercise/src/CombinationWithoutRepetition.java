301dcbad360bf40b904f11a564a2d68cf0425f21
doychinivanov <doychinivn@gmail.com>
2021-12-03 17:40:07 +0200

    solutions added

---
+++ Algorithms with Java/Fundamentals/03. Recursion and Combinatorial Algorithms - Exercise/src/CombinationWithoutRepetition.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class CombinationWithoutRepetition {
+    private static int elementsCount;
+    private static int slots;
+    private static int[] combinations;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        elementsCount = Integer.parseInt(reader.readLine());
+        slots = Integer.parseInt(reader.readLine());
+        combinations = new int[slots];
+
+        createCombinations(0, 1);
+    }
+
+    private static void createCombinations(int index, int start) {
+        if(index == slots) {
+            printArr();
+            return;
+        }
+
+        for (int i = start; i <= elementsCount ; i++) {
+            combinations[index] = i;
+
+            createCombinations(index + 1, i + 1);
+        }
+    }
+
+    private static void printArr() {
+        String output = Arrays
+                .stream(combinations)
+                .mapToObj(Integer::toString)
+                .collect(Collectors.joining(" "));
+
+        System.out.println(output);
+    }
+}
--- Algorithms with Java/Fundamentals/03. Recursion and Combinatorial Algorithms - Exercise/src/TowersOfHanoi.java
+++ Algorithms with Java/Fundamentals/03. Recursion and Combinatorial Algorithms - Exercise/src/TowersOfHanoi.java
@@ -1,6 +1,58 @@
-public class TowersOfHanoi {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.stream.Collectors;
 
-    public static void main(String[] args) {
-        
+public class TowersOfHanoi {
+    private static StringBuilder output = new StringBuilder();
+    private static Deque<Integer> source = new ArrayDeque<>();
+    private static Deque<Integer> spare = new ArrayDeque<>();
+    private static Deque<Integer> destination = new ArrayDeque<>();
+
+    private static int steps = 1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int disk = Integer.parseInt(reader.readLine());
+
+        for (int i = disk; i >= 1 ; i--) {
+            source.push(i);
+        }
+
+        output.append(printRods()).append(System.lineSeparator());
+        solvePuzzle(disk, source, destination, spare);
+        System.out.println(output.toString().trim());
+    }
+
+    private static void solvePuzzle(int disk, Deque<Integer> source, Deque<Integer> destination, Deque<Integer> spare) {
+        if (disk == 1) {
+            destination.push(source.pop());
+            output.append(String.format("Step #%d: Moved disk" + System.lineSeparator(), steps++));
+            output.append(printRods()).append(System.lineSeparator());
+            return;
+        }
+
+        solvePuzzle(disk - 1, source, spare, destination);
+        solvePuzzle(1, source, destination, spare);
+        solvePuzzle(disk - 1, spare, destination, source);
+
+    }
+
+    public static String printRods(){
+        return String.format("Source: %s%n" +
+                "Destination: %s%n" +
+                "Spare: %s%n", formatRod(source), formatRod(destination), formatRod(spare));
+    }
+
+    private static String formatRod(Deque<Integer> stack) {
+        return stack
+                .stream()
+                .sorted(Comparator.reverseOrder())
+                .map(String::valueOf)
+                .collect(Collectors.joining(", "));
     }
 }
